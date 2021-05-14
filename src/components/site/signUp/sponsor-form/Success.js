234e8a51d82be0bb58dbd2a4615281994a2d736f
Yuzhi <limruby97@gmail.com>
2021-05-14 23:47:33 +0800

    merge with kale's logout function

--- src/components/site/signUp/sponsor-form/Success.js
+++ src/components/site/signUp/sponsor-form/Success.js
@@ -5,12 +5,7 @@ export class Success extends Component {
     render() {
         return (
             <div>
-<<<<<<< HEAD
                 <br></br>
-=======
-                <h3><strong>Account Created Successfully!</strong></h3>
-				<br></br>
->>>>>>> c9c0eec4dad69e4e355633a815a43640aff67b56
 				<Link to="/sign_in">
 					<div className="text-center"><a className="btn btn-primary text-uppercase js-scroll-trigger" href="/sign_up">Please Sign In Here</a></div>
 				</Link>
