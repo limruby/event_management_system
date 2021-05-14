c9c0eec4dad69e4e355633a815a43640aff67b56
limkale <limkale1997@gmail.com>
2021-05-14 23:18:24 +0800

    with logout

--- src/components/site/navbar/index.js
+++ src/components/site/navbar/index.js
@@ -57,7 +57,7 @@ window.addEventListener('resize', showButton);
     <IconContext.Provider value={{color:'#000'}}>
 
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand href="#home"> <Link to='home_section_id' className="navbar-logo" onClick={closeMobileMenu}>
+      <Navbar.Brand href="#home"> <Link to='#' className="navbar-logo" onClick={closeMobileMenu}>
            <img src={logo} height="50px" width="200px" alt="" />
         </Link></Navbar.Brand>
       <Nav className="ml-auto">    
