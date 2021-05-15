f8445e08157d17e4593ddcfede90a133b2021291
limkale <limkale1997@gmail.com>
2021-05-15 16:34:02 +0800

    success page

--- src/components/site/signUp/competitor-form/Success.js
+++ src/components/site/signUp/competitor-form/Success.js
@@ -5,6 +5,7 @@ export class Success extends Component {
     render() {
         return (
             <div>
+			<h3><strong>Account Created Successfully!</strong></h3>
                 <br></br>
 				<Link to="/sign_in">
 					<div className="text-center"><a className="btn btn-primary text-uppercase js-scroll-trigger" href="/sign_up">Please Sign In Here</a></div>
