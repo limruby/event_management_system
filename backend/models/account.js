bdfdade8ef5fad2a38315562e2de7d9c48db9c64
alexia97a <74533213+alexia97a@users.noreply.github.com>
2021-05-26 17:10:24 +0800

    Admin login

--- backend/models/account.js
+++ backend/models/account.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
 const accountSchema = new Schema({
   role:{
@@ -22,6 +22,27 @@ const accountSchema = new Schema({
 
 
 
+
+bcrypt.hash('dinowex99admin', 10, function(err, hashedPassword){
+    if(err){
+      res.json({
+        error:err
+      })
+    }
+
+    Account.insertMany([
+        { role: 'Admin', email: 'admin@dinowex.com', password:hashedPassword},
+    ],
+        { ordered: false}
+      
+
+    ).then(function(){
+        console.log("Account data inserted")  // Success
+    }).catch(function(error){
+        console.log(error)      // Failure
+    });
+
+});
 
 
 const Account = mongoose.model('Account', accountSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const multer  = require('multer');
+const path = require ('path');
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
 
 app.use(cors());
 app.use(express.json({limit:'50mb'}));
@@ -17,29 +21,21 @@ const connection = mongoose.connection;
 connection.once('open', ()=>{
 	console.log("MongoDB database connection established")
 })
-app.get('/api/hello', (req, res) => {
-	res.send({ express: 'Hello From Express' });
-  });
-  app.post('/api/world', (req, res) => {
-	console.log(req.body);
-	res.send(
-	  `I received your POST request. This is what you sent me: ${req.body.post}`,
-	);
-  });
+
 //add routes
 const accountsRouter = require('./routes/accounts');
-app.use('/api/accounts', accountsRouter)
+app.use('/accounts', accountsRouter)
 
 const rolesRouter = require('./routes/roles');
-app.use('/api/roles', rolesRouter)
+app.use('/roles', rolesRouter)
 
 const competitorsRouter = require('./routes/competitors');
-app.use('/api/competitors', competitorsRouter)
+app.use('/competitors', competitorsRouter)
 
 const sponsorsRouter = require('./routes/sponsors');
-app.use('/api/sponsors', sponsorsRouter)
+app.use('/sponsors', sponsorsRouter)
 
 app.listen(port, () => {
-	console.log('Now starting at http://localhost:5000/api/hello');
+	console.log('Now starting at port: 5000');
 });
 
