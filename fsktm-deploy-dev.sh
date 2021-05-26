3a06b2e1d61615a5748fbf2a6842f813ce622198
WAI XIONG CHEE <wai.xiong@setel.my>
2021-05-26 12:33:40 +0800

    fsktm sh

--- fsktm-deploy-dev.sh
+++ fsktm-deploy-dev.sh
@@ -1,7 +1,7 @@
 npm install
 npm run build
 zip ./build.zip ./build -r
-scp ./build.zip dinowex@10.100.12.250:~/web/build.zip
+scp ./build.zip dinowex@10.100.12.250:~/client/build.zip
 scp ./docker-compose.yaml dinowex@10.100.12.250:~/client/docker-compose.yaml
 scp ./Dockerfile dinowex@10.100.12.250:~/client/Dockerfile
 rm -r ./build.zip
