aec88b26c5ecfd40d71c661f1e5b6c76eabb0a20
WAI XIONG CHEE <wai.xiong@setel.my>
2021-05-26 23:15:14 +0800

    update fsk serve

--- backend/fsktm-deploy-dev.sh
+++ backend/fsktm-deploy-dev.sh
@@ -1,6 +1,6 @@
 docker build -t vexs/backend . 
 docker save vexs/backend > vexs.tar
 scp ./vexs.tar dinowex@103.18.2.220:~/server/vexs.tar
-scp ./docker-compose.yaml dinowex@103.18.2.220:~/server/docker-compose.yaml
+scp ./docker-compose-fsktm.yaml dinowex@103.18.2.220:~/server/docker-compose.yaml
 rm -r ./vexs.tar
 docker rmi vexs/backend
--- fsktm-deploy-dev.sh
+++ fsktm-deploy-dev.sh
@@ -2,6 +2,6 @@ npm install
 npm run build
 zip ./build.zip ./build -r
 scp ./build.zip dinowex@103.18.2.220:~/client/build.zip
-scp ./docker-compose.yaml dinowex@103.18.2.220:~/client/docker-compose.yaml
+scp ./docker-compose-fsktm.yaml dinowex@103.18.2.220:~/client/docker-compose.yaml
 scp ./Dockerfile dinowex@103.18.2.220:~/client/Dockerfile
 rm -r ./build.zip
