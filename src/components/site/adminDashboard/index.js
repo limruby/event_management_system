ca69ace66200674b6869432ed5e75887f00925dc
alexia97a <74533213+alexia97a@users.noreply.github.com>
2021-05-29 22:36:06 +0800

    Admin

---
+++ src/components/site/adminDashboard/index.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import axiosInstance from '../../../utils/axiosConfig.js';
+ 
+ 
+function AdminDashboard() {
+ 
+////////////////////get login user info (REPLACE THIS)  /////////////////////
+
+ 
+ 
+  return (
+   <>
+     
+    <div>
+      
+      <p>uuboiiasojoa</p>
+
+    </div>  
+ 
+    
+ 
+   
+    
+   
+  </>
+  );
+ 
+}
+ 
+export default AdminDashboard;
