d26dd80013608e9e98f6c2d950782524d1f6d650
WAI XIONG CHEE <wai.xiong@setel.my>
2021-06-05 13:31:38 +0800

    move deployment

--- backend/fsktm-deploy-dev.sh
+++ backend/fsktm-deploy-dev.sh
@@ -1,2 +1,2 @@
-scp ./vexsdev.tar dinowex@103.18.2.220:~/vexsdev/server/vexsdev.tar
 scp ./docker-compose-fsktm.yaml dinowex@103.18.2.220:~/vexsdev/server/docker-compose.yaml
+scp ./vexsdev.tar dinowex@103.18.2.220:~/vexsdev/server/vexsdev.tar
