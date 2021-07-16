d4341f3ca05f81c59f7e65c327afc54c610d8d37
alexia97a <74533213+alexia97a@users.noreply.github.com>
2021-07-16 23:02:08 +0800

    deleteAcc

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -9,5 +9,6 @@ router.get('/read', AccountController.read);
 router.get('/readAll', AccountController.readAll);
 router.post('/update', authenticate, AccountController.update); 
 router.get('/readAdmin', AccountController.readAdmin); 
+router.get('/deleteOne', AccountController.deleteOne); 
 
 module.exports = router;
